6fc03daf5af58d7701285e2045f3585fcb6a3c19
Ed Henderson <ed@eeweb.com>
2015-04-05 14:51:51 -0600

    Add real db on prod.

--- django_root/sharpertool/settings/prod.py
+++ django_root/sharpertool/settings/prod.py
@@ -6,6 +6,18 @@ print("Loading production settings.")
 
 from .base import *
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sharpertool',
+        'USER': 'sharper_user',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+
 RAVEN_CONFIG = {
     'dsn': 'https://53acba3177ec4ce4be06cca3ba5602f3:4ee592373d8c41968c730327cfb6b808@app.getsentry.com/41135',
 }
