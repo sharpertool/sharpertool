6b9585f2b1e6c83c0de3e9d12cd603933b5ec6cb
Ed Henderson <ed@eeweb.com>
2015-04-05 14:57:35 -0600

    Redirect static root.

--- django_root/sharpertool/settings/base.py
+++ django_root/sharpertool/settings/base.py
@@ -59,7 +59,7 @@ STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
-STATIC_URL = "/site_media/static/"
+STATIC_URL = "/static/"
 
 # Additional locations of static files, if any.
 STATICFILES_DIRS = [
